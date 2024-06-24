d3c8a384bd5662637f670ef376848527e89a0486
marlon moratti <moratti27@gmail.com>
2024-06-24 17:21:38 -0300

    adding torch model functions

---
+++ src/model.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class CNN(nn.Module):
+    def __init__(self, model_arch, n_outputs, activation=None, pretrained=True, freeze_conv=True):
+        super(CNN, self).__init__()
+        
+        if model_arch == 'densenet121':
+            weights = models.DenseNet121_Weights.DEFAULT if pretrained else None
+            self.model = models.densenet121(weights=weights)
+            in_features = self.model.classifier.in_features
+            self.model.classifier = nn.Linear(in_features, n_outputs)
+
+        elif model_arch == 'resnet18':
+            weights = models.ResNet18_Weights.DEFAULT if pretrained else None
+            self.model = models.resnet18(weights=weights)
+            in_features = self.model.fc.in_features
+            self.model.fc = nn.Linear(in_features, n_outputs)
+
+        elif model_arch == 'vgg16':
+            weights = models.VGG16_Weights.DEFAULT if pretrained else None
+            self.model = models.vgg16(weights=weights)
+            in_features = self.model.classifier[-1].in_features
+            self.model.classifier[-1] = nn.Linear(in_features, n_outputs)
+
+        else:
+            raise NotImplementedError(
+                f'The "{model_arch}" model is not implemented. Please choose one'
+                ' of the following: "densenet121", "resnet18" or "vgg16".'
+            )
+
+        self.activation = activation or nn.Identity()
+
+        if freeze_conv:
+            self._freeze_convolutional(model_arch)
+
+    def forward(self, x):
+        x = self.model(x)
+        x = self.activation(x)
+        return x
+
+    def save(self, path):
+        torch.save(self.state_dict(), path)
+
+    def load(self, path):
+        self.load_state_dict(torch.load(path))
+
+    def _freeze_convolutional(self, model_arch):
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        parameters = None
+        if model_arch in ['densenet121', 'vgg16']:
+            parameters = self.model.classifier.parameters()
+        elif model_arch == 'resnet18':
+            parameters = self.model.fc.parameters()
+        else:
+            raise NotImplementedError(
+                f'The "{model_arch}" model is not implemented. Please choose one'
+                ' of the following: "densenet121", "resnet18" or "vgg16".'
+            )
+
+        for param in parameters:
+            param.requires_grad = True
---
+++ src/utils.py
@@ -0,0 +1,97 @@
+from tqdm import tqdm
+import numpy as np
+import torch
+
+def train_epoch(model, epoch, max_epoch, criterion, optimizer, data_loader, device):
+    model.to(device)
+    model.train()
+
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    with tqdm(total=len(data_loader)) as pbar:
+        for i, (inputs, labels) in enumerate(data_loader, 1):
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            # Backward pass
+            loss.backward()
+            optimizer.step()
+
+            ## statistics
+            running_loss += loss.item()
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+            pbar.set_description(
+                f'[ Training ]'
+                f'[ Epoch: {epoch+1:02d}/{max_epoch:02d}, '
+                f'Loss: {running_loss/i:.6f}, '
+                f'Accuracy: {correct/total*100:.2f}% ]'
+            )
+            pbar.update()
+
+    return model
+
+def evaluate_model(model, criterion, data_loader, device):
+    model.to(device)
+    model.eval()
+
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    y_true = []
+    y_pred = []
+
+    with torch.no_grad():
+        with tqdm(total=len(data_loader)) as pbar:
+            for i, (inputs, labels) in enumerate(data_loader, 1):
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                # Forward pass
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                ## statistics
+                running_loss += loss.item()
+                _, predicted = torch.max(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+                y_true.extend(labels)
+                y_pred.extend(predicted)
+
+                pbar.set_description(
+                    f'[ Testing ]'
+                    f'[ Loss: {running_loss/i:.6f}, '
+                    f'Accuracy: {correct/total*100:.2f}% ]'
+                )
+                pbar.update()
+    
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+
+    return y_true, y_pred
+
+
+def train_model(model, criterion, optimizer, train_loader, device, max_epochs, val_loader=None):
+    for epoch in range(max_epochs):
+        model = train_epoch(model, epoch, max_epochs, criterion, optimizer, train_loader, device)
+
+        # TODO: save checkpoints
+
+        if val_loader is not None:
+            y_true, y_pred = evaluate_model(model, criterion, val_loader, device)
+
+def test_model(model, criterion, test_loader, device):
+    y_true, y_pred = evaluate_model(model, criterion, test_loader, device)
+
+    # TODO: save test
