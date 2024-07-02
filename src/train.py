f8668e35ae84ad2badfcbf4bb94330347fd18645
marlon moratti <moratti27@gmail.com>
2024-07-02 10:24:15 -0300

    some changes

--- src/train.py
+++ src/train.py
@@ -1,8 +1,8 @@
+from collections import OrderedDict, defaultdict
 from tqdm import tqdm
 import numpy as np
 import torch
 import os
-import pandas as pd
 
 
 def save_checkpoint(filename, epoch, model, criterion, optimizer, loss, loss_val):
@@ -34,11 +34,10 @@ def train_epoch(model, epoch, max_epoch, criterion, optimizer, data_loader, devi
     model.train()
 
     running_loss = 0.0
-    correct = 0
     total = 0
 
-    with tqdm(total=len(data_loader)) as pbar:
-        for i, (inputs, labels, _) in enumerate(data_loader, 1):
+    with tqdm(data_loader) as tqdm_train:
+        for inputs, labels, _ in tqdm_train:
             inputs, labels = inputs.to(device), labels.to(device)
 
             optimizer.zero_grad()
@@ -51,64 +50,65 @@ def train_epoch(model, epoch, max_epoch, criterion, optimizer, data_loader, devi
             loss.backward()
             optimizer.step()
 
-            ## statistics
-            running_loss += loss.item()
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            ## Statistics
+            running_loss += loss.item() * inputs.size(0)
+            total += inputs.size(0)
 
-            pbar.set_description(
+            tqdm_train.set_description(
                 f"[ Training ]"
                 f"[ Epoch: {epoch+1:02d}/{max_epoch:02d}, "
-                f"Loss: {running_loss/i:.6f}, "
-                f"Accuracy: {correct/total*100:.2f}% ]"
+                f"Loss: {running_loss/total:.6f} ]"
             )
-            pbar.update()
 
-    avg_loss = running_loss / len(data_loader)
+    avg_loss = running_loss / len(data_loader.dataset)
     return avg_loss
 
 
-def evaluate_model(model, criterion, data_loader, device):
+def evaluate_model(model, criterion, data_loader, device, save_test=False):
     model.to(device)
     model.eval()
 
     running_loss = 0.0
-    correct = 0
     total = 0
 
     y_true = []
     y_pred = []
-    paths = []
+    paths  = []
 
     with torch.no_grad():
-        with tqdm(total=len(data_loader)) as pbar:
-            for i, (inputs, labels, path) in enumerate(data_loader, 1):
+        with tqdm(data_loader) as tqdm_eval:
+            for inputs, labels, path in tqdm_eval:
                 inputs, labels = inputs.to(device), labels.to(device)
 
                 # Forward pass
                 outputs = model(inputs)
                 loss = criterion(outputs, labels)
 
-                ## statistics
-                running_loss += loss.item()
-                _, predicted = torch.max(outputs, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+                ## Statistics
+                running_loss += loss.item() * inputs.size(0)
+                total += inputs.size(0)
 
-                y_true.append(labels.cpu().numpy())
-                y_pred.append(predicted.cpu().numpy())
-                paths.append(path)
+                if save_test:
+                    y_true.append(labels.cpu())
+                    y_pred.append(outputs.cpu())
+                    paths.extend(path)
 
-                pbar.set_description(
+                tqdm_eval.set_description(
                     f"[ Testing ]"
-                    f"[ Loss: {running_loss/i:.6f}, "
-                    f"Accuracy: {correct/total*100:.2f}% ]"
+                    f"[ Loss: {running_loss/total:.6f} ]"
                 )
-                pbar.update()
 
-    avg_loss = running_loss / len(data_loader)
-    return avg_loss, (np.concatenate(paths), np.concatenate(y_true), np.concatenate(y_pred))
+    avg_loss = running_loss / len(data_loader.dataset)
+
+    result = None
+    if save_test:
+        result = OrderedDict([
+            ('y_true', torch.cat(y_true)),
+            ('y_pred', torch.cat(y_pred)),
+            ('paths', paths)
+        ])
+
+    return avg_loss, result
 
 
 def train_model(
@@ -122,29 +122,29 @@ def train_model(
     epoch=None,
     checkpoint_dir="./checkpoints",
 ):
-    best_loss = np.inf
 
-    for epoch in range(epoch or 0, max_epochs):
-        loss = train_epoch(
-            model, epoch, max_epochs, criterion, optimizer, train_loader, device
-        )
+    best_loss = np.inf
+    history = defaultdict(list)
+
+    epoch = epoch if isinstance(epoch, int) else 0
+    for epoch in range(epoch, max_epochs):
+        loss = train_epoch(model, epoch, max_epochs, criterion, optimizer, train_loader, device)
         loss_val, _ = evaluate_model(model, criterion, val_loader, device)
 
+        history['loss'].append(loss)
+        history['loss_val'].append(loss_val)
+
         filename = os.path.join(checkpoint_dir, "last_checkpoint.pth")
-        save_checkpoint(
-            filename, epoch + 1, model, criterion, optimizer, loss, loss_val
-        )
+        save_checkpoint(filename, epoch + 1, model, criterion, optimizer, loss, loss_val)
 
         if loss_val < best_loss:
             best_loss = loss_val
             filename = os.path.join(checkpoint_dir, "best_checkpoint.pth")
-            save_checkpoint(
-                filename, epoch + 1, model, criterion, optimizer, loss, loss_val
-            )
+            save_checkpoint(filename, epoch + 1, model, criterion, optimizer, loss, loss_val)
+    
+    return history
 
 
-def test_model(model, criterion, test_loader, device, filename="test_predictions.csv"):
-    _, eval = evaluate_model(model, criterion, test_loader, device)
-
-    df = pd.DataFrame(zip(*eval), columns=["img_path", "label", "prediction"])
-    df.to_csv(filename, index=False)
+def test_model(model, criterion, test_loader, device, filename="test_predictions.pth"):
+    _, result = evaluate_model(model, criterion, test_loader, device, save_test=True)
+    torch.save(result, filename)
