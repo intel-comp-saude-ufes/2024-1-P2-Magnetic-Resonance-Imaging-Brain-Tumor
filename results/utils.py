d592ea290cd72ee744282b260db38d130fed8f6e
Nilo Monteiro <nilo.monteiro@edu.ufes.br>
2024-07-14 22:40:48 -0300

    :test_tube: Updated test notebooks

--- results/utils.py
+++ results/utils.py
@@ -1,7 +1,3 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as nnF
-
 import os
 
 import pandas as pd
@@ -11,24 +7,23 @@ import torch
 from matplotlib.axes import Axes
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
-from PIL import Image
 import albumentations as A
+from PIL import Image
+
+from sklearn.metrics import balanced_accuracy_score, recall_score
 
 
-class Dice(nn.Module):
-    def __init__(self, smooth=1e-6, dims=(-2, -1)):
-        super(Dice, self).__init__()
-        self.smooth = smooth
-        self.dims = dims
+def _dice(output, target):
+    dims = (-2, -1)
+    smooth = 1e-6
 
-    def forward(self, output, target):
-        output = output.sigmoid()
-        output[output > 0.5] = 1
+    output = output.sigmoid()
+    output = torch.where(output > 0.5, 1, 0)
 
-        tp = (output * target).sum(dim=self.dims)
-        fp = (output * (1 - target)).sum(dim=self.dims)
-        fn = ((1 - output) * target).sum(dim=self.dims)
-        return ((2 * tp + self.smooth) / (2 * tp + fp + fn + self.smooth)).mean()
+    tp = (output * target).sum(dim=dims)
+    fp = (output * (1 - target)).sum(dim=dims)
+    fn = ((1 - output) * target).sum(dim=dims)
+    return ((2 * tp + smooth) / (2 * tp + fp + fn + smooth)).mean()
 
 
 def read_test(run_folder: str):
@@ -51,90 +46,107 @@ def show_image(y_true: torch.Tensor, y_pred: torch.Tensor, path, ax: Axes):
     ax.imshow(y_true, cmap=ListedColormap(colors=[(0.0, 0.0, 0.0, 0.5), (0.0, 1.0, 0.0, 1.0)]), alpha=0.3)
 
 
-class Results:
-    def __init__(self, files: dict[str, dict]) -> None:
-        self.files = files
-        self.scoring = Dice()
-        self.scores = {}
-
-    def show_patient_results(self, patient_info, title):
-        img_idx, mean, std = patient_info
-        n_imgs = len(img_idx)
-        cols = min(n_imgs, 6)
-        rows = (n_imgs // (cols + 1)) + 1
-        fig, axes = plt.subplots(rows, cols, sharey=True, figsize=(cols * 3, 3.1 * rows))
-        fig.suptitle(title.format(mean, std))
-
-        if cols == 1:
-            axes = np.array(axes)
-        axes = axes.flatten()
-        for ax in axes:
-            ax.set_axis_off()
-
-        for ax, idx in zip(axes, img_idx):
-            y_pred, y_true, img = self._get_item(idx, path=True)
-            ax.set_title("{:02.02f}%".format(self.scoring(y_pred, y_true).item() * 100))
-            show_image(y_true, y_pred, img, ax)
-
-    def calculate_patient_scores(self):
-        n_indices = len(self.fold["paths"])
-        available_indices = set(range(len(self.fold["paths"])))
-        p_scores = []
-
-        while len(available_indices) != 0:
-            pid_fold = available_indices.pop()
-            p_img_ref = self.fold["paths"][pid_fold]
-            p_idx = get_patient_info(p_img_ref, self.fold)
-
-            # calcula os resultados de cada imagem do paciente
-            f1 = np.array([self.scoring(*self._get_item(i)) for i in range(n_indices) if i in p_idx])
-            p_scores.append((p_idx, f1.mean(), f1.std()))
-
-            available_indices -= set(p_idx)
-
-        return p_scores
-
-    def show(self):
-        for name, fold in self.files.items():
-            self.fold = fold
-
-            score = self.scoring(*self._get_item()).item() * 100
-            print(name, "F1 macro: {:02.02f}%".format(score))
-            self.scores[name] = score
-
-            p_scores = self.calculate_patient_scores()
-            p_scores.sort(key=lambda x: x[1])
-
-            worst_patient = p_scores[0]
-            self.show_patient_results(worst_patient, name + " worst f1 macro score: {:02.02f} +- {:02.04f}")
-
-            best_patient = p_scores[-1]
-            self.show_patient_results(best_patient, name + " best f1 macro score: {:02.02f} +- {:02.04f}")
-
-    def _get_item(self, id=None, path: bool = False):
-        if id is not None:
-            if path:
-                return self.fold["y_pred"][id], self.fold["y_true"][id], self.fold["paths"][id]
-            return self.fold["y_pred"][id], self.fold["y_true"][id]
-
-        if path:
-            return self.fold["y_pred"], self.fold["y_true"], self.fold["paths"]
-        return self.fold["y_pred"], self.fold["y_true"]
-
-    def boxplot(self):
-        scores = np.array(list(self.scores.values()))
-        print(scores.mean(), scores.std())
-
-
 info = pd.read_csv("../dataset/data/info.csv")
+info["label"] = info["label"].map({"glioma": 0, "meningioma": 1, "pituitary": 2})
 
 
-def get_patient_info(ref_path, fold, return_label=False):
+def show_patient_results(idx, outputs, masks, labels, img_paths, title):
+    n_imgs = len(idx)
+    cols = min(n_imgs, 6)
+    rows = (n_imgs // (cols + 1)) + 1
+
+    fig, axes = plt.subplots(rows, cols, sharey=True, figsize=(cols * 3, 3.1 * rows))
+    fig.suptitle(title)
+
+    if cols == 1:
+        axes = np.array(axes)
+    axes = axes.flatten()
+    for ax in axes:
+        ax.set_axis_off()
+
+    idx = np.array(idx)
+    for ax, seg, y_pred, mask, y_true, i in zip(axes, outputs["seg"][idx], outputs["class"][idx], masks[idx], labels[idx], idx):
+        dice = _dice(seg, mask) * 100
+        ax.set_title("{:02.02f}%".format(dice))
+        show_image(mask, seg, img_paths[i], ax)
+
+
+def calculate_patient_scores(outputs, masks, labels, img_paths):
+    n_indices = len(outputs)
+    available_indices = set(range(n_indices))
+    p_scores = []
+
+    while len(available_indices) != 0:
+        pid_fold = available_indices.pop()
+        p_img_ref = img_paths[pid_fold]
+        p_idx, _, _ = get_patient_info(p_img_ref, img_paths)
+
+        dice = _dice(outputs["seg"][p_idx], masks[p_idx])
+        acc = recall_score(labels[p_idx], np.argmax(outputs["class"][p_idx], 1), average="macro")
+        p_scores.append((p_idx, dice, acc))
+
+        available_indices -= set(p_idx)
+
+    return p_scores
+
+
+def show(files):
+    scores = {}
+    for name, fold in files.items():
+        outputs = fold["y_pred"]
+        img_paths = fold["paths"]
+        masks, labels = get_all_info(img_paths)
+
+        dice = _dice(outputs["seg"], masks)
+
+        y_pred = torch.argmax(outputs["class"], 1)
+        acc = recall_score(labels, y_pred, average="macro")
+
+        print(name)
+        print("Dice (F1 macro): {:02.02f}%".format(dice * 100))
+        print("Balanced Accuracy: {:02.02f}%".format(acc * 100))
+        scores[name] = (dice, acc)
+
+        p_scores = calculate_patient_scores(outputs, masks, labels, img_paths)
+        p_scores.sort(key=lambda x: x[1])
+
+        w_idx, w_score, _ = p_scores[0]
+        show_patient_results(w_idx, outputs, masks, labels, img_paths, name + " worst dice score: {:02.02f}".format(w_score))
+
+        b_idx, b_score, _ = p_scores[-1]
+        show_patient_results(b_idx, outputs, masks, labels, img_paths, name + " best dice score: {:02.02f}".format(b_score))
+
+    return scores
+
+
+def get_all_info(img_paths):
+    labels = []
+    masks = []
+    for path in img_paths:
+        m, l = get_item_info(path)
+        labels.append(l)
+        masks.append(m)
+    masks = np.array(masks)
+    labels = np.array(labels)
+    return masks, labels
+
+
+def get_item_info(ref_path: str):
+    item_info = info.loc[info["image_path"] == ref_path]
+
+    mask_path = item_info["mask_path"].item()
+    label = item_info["label"].item()
+
+    mask = resize(image=np.array(Image.open("../" + mask_path)))
+    return mask["image"], label
+
+
+def get_patient_info(ref_path, img_paths):
     pid = info.loc[info["image_path"] == ref_path, "id"].item()
-    imgs = info.loc[info["id"] == pid, "image_path"].tolist()
+    patient_info = info.loc[info["id"] == pid]
 
-    if return_label:
-        label = info.loc[info["id"] == pid, "label"][0].item()
-        return [i for i, path in enumerate(fold["paths"]) if path in imgs], label
+    imgs = patient_info["image_path"].tolist()
+    masks = patient_info["mask_path"].tolist()
 
-    return [i for i, path in enumerate(fold["paths"]) if path in imgs]
+    idx = [i for i, path in enumerate(img_paths) if path in imgs]
+    return idx, imgs, masks
